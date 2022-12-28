6b916b3f3702b592794f53309b993ade9d4f6432
MythChaser <121262787+MythChaser@users.noreply.github.com>
2022-12-28 17:17:43 +0530

    Update batch.py

--- batch.py
+++ batch.py
@@ -1,192 +1,192 @@
-import mysql.connector
 import json
-from matplotlib import pyplot
-mydb = mysql.connector.connect(host = "localhost", user = "root", password = "*******", database = "batch")
-cursorObject = mydb.cursor()
+import csv
+import pandas
+from batch import createBatch
 
-def createBatch(batch_name):
-    batch_id = batch_name[:3] + batch_name[6:8]
-    department_name = batch_id[:3]
-    print("Enter courses in batch: ")
-    courses_L = []
-    while(True):
-        course = input("Enter course ID (to stop enter STOP): ")
-        if(course.upper() == "STOP"):
+def createStudent(student_id, name):
+    class_roll_number = int(student_id[5:7])
+    batch_id = student_id[:5]
+    data = [student_id, name, class_roll_number, batch_id]
+    csv_reader = []
+    with open("student.csv", "r", newline = "\n") as f:
+        csv_reader = list(csv.reader(f, delimiter=","))
+    with open("student.csv", "a", newline = "\n") as f:
+        for i in range(0, len(csv_reader)):
+            if(csv_reader[i][0] == student_id):
+                print("Student ID already exists")
+                return
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(data)
+    with open("batch.csv", "r", newline = "\n") as f:
+        csv_reader = list(csv.reader(f, delimiter=","))
+    check = 0
+    for i in range(0, len(csv_reader)):
+        if(csv_reader[i][0] == batch_id):
+            check = 1
+            if(csv_reader[i][4] == ""):
+                csv_reader[i][4] = csv_reader[i][4] + student_id
+            else:
+                csv_reader[i][4] = csv_reader[i][4] + ":" + student_id
+            df = pandas.read_csv("batch.csv")
+            df.loc[i-1, "list_of_students"] = csv_reader[i][4]
+            df.to_csv("batch.csv", index = False)
+    if(check == 0):
+        print("Batch does not exist.... Creating new batch")
+        batch_name = batch_id[:3] + " 20" + batch_id[3:] + "-" + str(int(batch_id[3:]) + 4)
+        createBatch(batch_name)
+    with open("batch.csv", "r", newline = "\n") as f:
+        csv_reader = list(csv.reader(f, delimiter=","))
+    courses = []
+    for i in range(0, len(csv_reader)):
+        if(csv_reader[i][0] == batch_id):
+            courses = list(csv_reader[i][3].split(":"))
+    with open("course.csv", "r", newline = "\n") as f:
+        csv_reader = list(csv.reader(f, delimiter=","))
+    for i in range(0, len(csv_reader)):
+        for j in range(0, len(courses)):
+            if(csv_reader[i][0] == courses[j]):
+                if(csv_reader[i][2] == ""):
+                    temp = {}
+                    temp[student_id] = 0
+                    csv_reader[i][2] = json.dumps(temp)
+                else:
+                    temp = json.loads(csv_reader[i][2])
+                    temp[student_id] = 0
+                    csv_reader[i][2] = json.dumps(temp)
+                df = pandas.read_csv("course.csv")
+                df.loc[i-1, "marks_obtained"] = csv_reader[i][2]
+                df.to_csv("course.csv", index = False)
+
+def updateStudent(ostudent_id):
+    csv_reader = []
+    with open("student.csv", "r", newline = "\n") as f:
+        csv_reader = list(csv.reader(f, delimiter=","))
+    check = 0
+    for i in range(0, len(csv_reader)):
+        if(csv_reader[i][0] == ostudent_id):
+            check = 1
             break
+    if(check == 0):
+        print("Student ID does not exist")
+        return
+    while(True):
+        print("Press 1 to update name")
+        print("Press 2 to update student ID")
+        print("Press 0 to exit")
+        x = int(input("Enter your choice: "))
+        if(x == 0):
+            break
+        elif(x == 1):
+            name = input("Enter updated name: ")
+            df = pandas.read_csv("student.csv")
+            df.loc[i-1, "Name"] = name
+            df.to_csv("student.csv", index = False)
+        elif(x == 2):
+            nstudent_id = input("Enter updated student ID: ")
+            df = pandas.read_csv("student.csv")
+            df.loc[i-1, "Student_ID"] = nstudent_id
+            df.to_csv("student.csv", index = False)
+            removeStudent(ostudent_id)
+            createStudent(nstudent_id, csv_reader[i][1])
+            ostudent_id = nstudent_id
+            with open("student.csv", "r", newline = "\n") as f:
+                csv_reader = list(csv.reader(f, delimiter=","))
         else:
-            courses_L.append(course)
-    temp = ":"
-    courses = (temp.join(courses_L))
-    mydb1 = mysql.connector.connect(host = "localhost", user = "root", password = "*******", database = "student")
-    cursorObject1 = mydb1.cursor()
-    cursorObject1.execute("SELECT * FROM student")
-    result1 = cursorObject1.fetchall()
-    student_ids = []
-    batch_ids = []
-    for i in range(0, len(result1)):
-        student_ids.append(result1[i][0])
-        batch_ids.append(result1[i][3])
-    student_id_L = []
-    for i in range(0, len(batch_ids)):
-        if(batch_ids[i] == batch_id):
-            student_id_L.append(student_ids[i])
-    student_id = (temp.join(student_id_L))
-    sql = "INSERT INTO batch (batch_id, batch_name, department_name, list_of_courses, list_of_students) VALUES (%s, %s, %s, %s, %s)"
-    val = (batch_id, batch_name, department_name, courses, student_id)
-    cursorObject.execute(sql, val)
-    mydb.commit()
+            print("Invalid input. Try again.")
 
-def viewStudents(batch_id):
-    cursorObject.execute("SELECT * FROM batch")
-    result = cursorObject.fetchall()
-    student_ids = []
-    batch_ids = []
-    for i in range(0, len(result)):
-        student_ids.append(result[i][4])
-        batch_ids.append(result[i][0])
+def removeStudent(student_id):
+    csv_reader = []
+    with open("student.csv", "r", newline = "\n") as f:
+        csv_reader = list(csv.reader(f, delimiter=","))
     check = 0
-    for i in range(0, len(batch_ids)):
-        if(batch_ids[i] == batch_id):
+    for i in range(0, len(csv_reader)):
+        if(csv_reader[i][0] == student_id):
             check = 1
-            student_id = list(student_ids[i].split(":"))
-            print("Students in batch: ")
-            for j in student_id:
-                print(j)
+            break
     if(check == 0):
-        print("Batch does not exist")
+        print("Student ID does not exist")
+        return
+    df = pandas.read_csv("student.csv")
+    df.set_index("Student_ID")
+    df = df.drop(df.index[i-1])
+    df.to_csv("student.csv", index = False)
+    with open("course.csv", "r", newline = "\n") as f:
+        csv_reader = list(csv.reader(f, delimiter=","))
+    for i in range(0, len(csv_reader)):
+        if(i == 0):
+            continue
+        temp = csv_reader[i][2]
+        temp = json.loads(temp)
+        if student_id in temp:
+            del temp[student_id]
+        csv_reader[i][2] = json.dumps(temp)
+    df = pandas.read_csv("course.csv")
+    for i in range(1, len(csv_reader)):
+        df.loc[i-1, "marks_obtained"] = csv_reader[i][2]
+    df.to_csv("course.csv", index = False)
+    with open("batch.csv", "r", newline = "\n") as f:
+        csv_reader = list(csv.reader(f, delimiter=","))
+    for i in range(0, len(csv_reader)):
+        if(i == 0):
+            continue
+        temp = list(csv_reader[i][4].split(":"))
+        if student_id in temp:
+            temp.remove(student_id)
+        a = ":"
+        csv_reader[i][4] = a.join(temp)
+    df = pandas.read_csv("batch.csv")
+    for i in range(1, len(csv_reader)):
+        df.loc[i-1, "list_of_students"] = csv_reader[i][4]
+    df.to_csv("batch.csv", index = False)
 
-def viewCourses(batch_id):
-    cursorObject.execute("SELECT * FROM batch")
-    result = cursorObject.fetchall()
-    course_ids = []
-    batch_ids = []
-    for i in range(0, len(result)):
-        course_ids.append(result[i][3])
-        batch_ids.append(result[i][0])
+def reportCard(student_id):
+    name = ""
+    csv_reader= []
+    with open("student.csv", "r", newline = "\n") as f:
+        csv_reader = list(csv.reader(f, delimiter=","))
     check = 0
-    for i in range(0, len(batch_ids)):
-        if(batch_ids[i] == batch_id):
+    for i in range(0, len(csv_reader)):
+        if(csv_reader[i][0] == student_id):
             check = 1
-            course_id = list(course_ids[i].split(":"))
-            print("Courses in batch: ")
-            for j in course_id:
-                print(j)
+            name = csv_reader[i][1]
+            break
     if(check == 0):
-        print("Batch does not exist")
+        print("Student ID does not exist")
+        return
+    f = open((student_id + ".txt"), "w")
+    a = "Student ID: " + student_id + "\n"
+    b = "Name: " + name + "\n"
+    f.writelines([a, b])
+    with open("course.csv", "r", newline = "\n") as fx:
+        csv_reader = list(csv.reader(fx, delimiter=","))
+    marks = []
+    subjects = []
+    for i in range(1, len(csv_reader)):
+        marks.append(json.loads(csv_reader[i][2]))
+        subjects.append(csv_reader[i][1])
+    total_marks = 0
+    divs = 0
+    for i in range(0, len(subjects)):
+        temp = marks[i]
+        if(isinstance(temp.get(student_id), int)):
+            subject_marks = "Marks in " + subjects[i] + ": " + str(temp.get(student_id)) + "% \n"
+            divs += 1
+            total_marks += temp.get(student_id)
+            f.write(subject_marks)
+    grade = "Grade obtained: " + gradeCheck(total_marks/divs) + " \n"
+    f.write(grade)
+    f.close()
 
-def viewPerformance(batch_id):
-    cursorObject.execute("SELECT * FROM batch")
-    result = cursorObject.fetchall()
-    student_ids = []
-    batch_ids = []
-    for i in range(0, len(result)):
-        student_ids.append(result[i][4])
-        batch_ids.append(result[i][0])
-    check = 0
-    for i in range(0, len(batch_ids)):
-        if(batch_ids[i] == batch_id):
-            check = 1
-            student_id = list(student_ids[i].split(":"))
-            mydb1 = mysql.connector.connect(host = "localhost", user = "root", password = "*******", database = "student")
-            cursorObject1 = mydb1.cursor()
-            cursorObject1.execute("SELECT * FROM student")
-            result1 = cursorObject1.fetchall()
-            student_ids1 = []
-            students = []
-            student_rolls = []
-            for j in range(0, len(result1)):
-                student_ids1.append(result1[j][0])
-                students.append(result1[j][1])
-                student_rolls.append(result1[j][2])
-            student_datas = [student_ids1, students, student_rolls]
-            for j in range(0, len(student_id)):
-                for k in range(0, len(student_ids1)):
-                    if(student_id[j] == student_ids1[k]):
-                        print("Student ID: " + student_datas[0][k])
-                        print("Student Name: " + student_datas[1][k])
-                        print("Student Roll Number: " + str(student_datas[2][k]))
-                        mydb2 = mysql.connector.connect(host = "localhost", user = "root", password = "*******", database = "course")
-                        cursorObject2 = mydb2.cursor()
-                        cursorObject2.execute("SELECT * FROM course")
-                        result2 = cursorObject2.fetchall()
-                        marks = []
-                        subjects = []
-                        for l in range(0, len(result2)):
-                            marks.append(json.loads(result2[l][2]))
-                            subjects.append(result2[l][1])
-                        total_marks = 0
-                        divs = 0
-                        for l in range(0, len(subjects)):
-                            temp = marks[l]
-                            if(isinstance(temp.get(student_datas[0][k]), int)):
-                                divs += 1
-                                total_marks += temp.get(student_datas[0][k])
-                        percentage = total_marks/divs
-                        print("Percentage obtained: " + str(percentage))
-                        print()
-    if(check == 0):
-        print("Batch does not exist")
-
-def pieChart(batch_id):
-    cursorObject.execute("SELECT * FROM batch")
-    result = cursorObject.fetchall()
-    student_ids = []
-    batch_ids = []
-    percentages = [">=90", ">=80", ">=70", ">=60", ">=50", "Failed"]
-    numbers = [0, 0, 0, 0, 0, 0]
-    for i in range(0, len(result)):
-        student_ids.append(result[i][4])
-        batch_ids.append(result[i][0])
-    check = 0
-    for i in range(0, len(batch_ids)):
-        if(batch_ids[i] == batch_id):
-            check = 1
-            student_id = list(student_ids[i].split(":"))
-            mydb1 = mysql.connector.connect(host = "localhost", user = "root", password = "*******", database = "student")
-            cursorObject1 = mydb1.cursor()
-            cursorObject1.execute("SELECT * FROM student")
-            result1 = cursorObject1.fetchall()
-            student_ids1 = []
-            for j in range(0, len(result1)):
-                student_ids1.append(result1[j][0])
-            for j in range(0, len(student_id)):
-                for k in range(0, len(student_ids1)):
-                    if(student_id[j] == student_ids1[k]):
-                        mydb2 = mysql.connector.connect(host = "localhost", user = "root", password = "*******", database = "course")
-                        cursorObject2 = mydb2.cursor()
-                        cursorObject2.execute("SELECT * FROM course")
-                        result2 = cursorObject2.fetchall()
-                        marks = []
-                        subjects = []
-                        for i in range(0, len(result2)):
-                            marks.append(json.loads(result2[i][2]))
-                            subjects.append(result2[i][1])
-                        total_marks = 0
-                        divs = 0
-                        for l in range(0, len(subjects)):
-                            temp = marks[l]
-                            if(isinstance(temp.get(student_ids1[k]), int)):
-                                divs += 1
-                                total_marks += temp.get(student_ids1[k])
-                        percentage = total_marks/divs
-                        if(percentage >= 90):
-                            numbers[0] += 1
-                        elif(percentage >= 80):
-                            numbers[1] += 1
-                        elif(percentage >= 70):
-                            numbers[2] += 1
-                        elif(percentage >= 60):
-                            numbers[3] += 1
-                        elif(percentage >= 50):
-                            numbers[4] += 1
-                        else:
-                            numbers[5] += 1
-            for i in range(len(numbers) - 1, -1, -1):
-                if(numbers[i] == 0):
-                    del numbers[i]
-                    del percentages[i]
-            fig = pyplot.figure(figsize =(10, 7))
-            pyplot.pie(numbers, labels = percentages)
-            pyplot.show()
-    if(check == 0):
-        print("Batch does not exist")
+def gradeCheck(a):
+    if(a >= 90):
+        return "A"
+    elif(a >= 80):
+        return "B"
+    elif(a >= 70):
+        return "C"
+    elif(a >= 60):
+        return "D"
+    elif(a >= 50):
+        return "E"
+    else:
+        return "F"
